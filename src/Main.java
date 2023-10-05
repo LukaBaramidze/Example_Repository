9e372cb9631da6f11e429c3bc48b6ec02db15599
LukaBaramidze <lukabaramidze253@gmail.com>
2023-10-05 20:20:42 +0400

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+
+        System.out.println("add to new branch");
+        System.out.println(1+1);
+
     }
+
+
 }
